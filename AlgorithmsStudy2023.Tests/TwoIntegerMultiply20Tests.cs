73c5baec152319da9b9269775d64fa7b4f73c40c
Lucas Ferreira <Lucas.Ferreira@tetrapak.com>
2023-09-16 12:02:28 +0200

    Implement multiply 20 solution

--- AlgorithmsStudy2023.Tests/TwoIntegerMultiply20Tests.cs
+++ AlgorithmsStudy2023.Tests/TwoIntegerMultiply20Tests.cs
@@ -4,6 +4,8 @@ public class TwoIntegerMultiply20Tests
 {
     [Theory]
     [InlineData(new[]{ 2, 10 })]
+    [InlineData(new[]{ 1, 2, 3, 4, 5, 6 })]
+    [InlineData(new[]{ 1, 2, 4, -1, 45, 10 })]
     public void Given_valid_input_should_return_expected_result(int[] integerArray)
     {
         var result = TwoIntegerMultiply20.Solution(integerArray);
--- AlgorithmsStudy2023/TwoIntegerMultiply20.cs
+++ AlgorithmsStudy2023/TwoIntegerMultiply20.cs
@@ -1,8 +1,24 @@
 ﻿namespace AlgorithmsStudy2023;
 public static class TwoIntegerMultiply20
 {
+    //1, 2, 3, 4, 5, 6
     public static int[] Solution(int[] integerArray)
     {
-        return new[] { integerArray[0], integerArray[1] };
+        //TODO: Implement an more optimal solution than this
+        for (var i = 0; i < integerArray.Length; i++)
+        {
+            if (integerArray[i] > 20 || integerArray[i] <= 0)
+                continue;
+            
+            var multiplier = integerArray[i];
+            
+            for (var j = i; j < integerArray.Length; j++)
+            {
+                if (multiplier * j == 20)
+                    return new[] { multiplier, j };
+            }
+        }
+        //Input arrays must be valid in this case (like 4,5 or 20,1 or 2,10)
+        return integerArray;
     }
 }
