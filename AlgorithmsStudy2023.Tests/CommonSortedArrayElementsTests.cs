164acd07abb146db750de37541e5911838c25e77
lucasfmarconi <lucasfmarconi@gmail.com>
2023-09-16 18:01:20 +0200

    Add common sorted array solution and tests

---
+++ AlgorithmsStudy2023.Tests/CommonSortedArrayElementsTests.cs
@@ -0,0 +1,13 @@
+﻿namespace AlgorithmsStudy2023.Tests;
+public class CommonSortedArrayElementsTests
+{
+    [Theory]
+    [InlineData(new[] { 1, 3, 4, 6, 7, 9 }, new[] { 1, 2, 4, 5, 9, 10 }, new[] { 1, 4, 9 })]
+    [InlineData(new[] { 1, 2, 9, 10, 11, 12 }, new[] { 0, 1, 2, 3, 4, 5, 8, 9, 10, 12, 14, 15 }, new[] { 1,2,9,10,12 })]
+    [InlineData(new[] { 0, 1, 2, 3, 4, 5 }, new[] {6, 7, 8, 9, 10, 11}, new int[]{})]
+    public void Given_valid_input_should_return_expected_result(int[] integerArrayOne, int[] integerArrayTwo, int[] integerArrayExpected)
+    {
+        var result = CommonSortedArrayElements.CommonElements(integerArrayOne, integerArrayTwo);
+        Assert.Equal(integerArrayExpected, result);
+    }
+}
---
+++ AlgorithmsStudy2023/CommonSortedArrayElements.cs
@@ -0,0 +1,35 @@
+﻿namespace AlgorithmsStudy2023;
+
+public class CommonSortedArrayElements
+{
+    public static int[] CommonElements(int[] integerArrayOne, int[] integerArrayTwo)
+    {
+        var commonElemetsArray = new List<int>();
+        var indexArrayOne = 0;
+        var indexArrayTwo = 0;
+
+        while (indexArrayOne < integerArrayOne.Length)
+        {
+            var elementInOne = integerArrayOne[indexArrayOne];
+            var elementInTwo = integerArrayTwo[indexArrayTwo];
+            if (elementInOne == elementInTwo)
+            {
+                commonElemetsArray.Add(elementInOne);
+                indexArrayOne++;
+                indexArrayTwo++;
+                continue;
+            }
+
+            if (elementInOne > elementInTwo)
+            {
+                indexArrayTwo++;
+                continue;
+            }
+
+            if (elementInOne < elementInTwo)
+                indexArrayOne++;
+        }
+
+        return commonElemetsArray.ToArray();
+    }
+}
