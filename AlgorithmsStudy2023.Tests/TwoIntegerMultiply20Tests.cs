0131c8ee5786ae03291bb2516c8f92dc0c82b71c
Lucas Ferreira <Lucas.Ferreira@tetrapak.com>
2023-09-16 11:24:47 +0200

    Add initial files to two integer multiply 20 solution

---
+++ AlgorithmsStudy2023.Tests/TwoIntegerMultiply20Tests.cs
@@ -0,0 +1,12 @@
+﻿namespace AlgorithmsStudy2023.Tests;
+
+public class TwoIntegerMultiply20Tests
+{
+    [Theory]
+    [InlineData(new[]{ 2, 10 })]
+    public void Given_valid_input_should_return_expected_result(int[] integerArray)
+    {
+        var result = TwoIntegerMultiply20.Solution(integerArray);
+        Assert.Equal(20, result[0] * result[1]);
+    }
+}
---
+++ AlgorithmsStudy2023/TwoIntegerMultiply20.cs
@@ -0,0 +1,8 @@
+﻿namespace AlgorithmsStudy2023;
+public static class TwoIntegerMultiply20
+{
+    public static int[] Solution(int[] integerArray)
+    {
+        return new[] { integerArray[0], integerArray[1] };
+    }
+}
