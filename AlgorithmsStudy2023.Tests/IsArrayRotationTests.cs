f9f4414ef7623e9ce4f59e3126b45cb71874900b
lucasfmarconi <lucasfmarconi@gmail.com>
2023-09-18 15:57:38 +0200

    Add is array rotation solution and tests

---
+++ AlgorithmsStudy2023.Tests/IsArrayRotationTests.cs
@@ -0,0 +1,17 @@
+﻿namespace AlgorithmsStudy2023.Tests;
+public class IsArrayRotationTests
+{
+    [Theory]
+    [InlineData(new[] { 1, 2, 3, 4, 5, 6, 7 }, new[] { 4, 5, 6, 7, 1, 2, 3 }, true)]
+    [InlineData(new[] { 1, 2, 3, 4, 5, 6, 7 }, new[] { 4, 5, 6, 7, 1, 2 }, false)]
+    [InlineData(new[] { 1, 2, 3, 4, 5, 6 }, new[] { 4, 5, 6, 7, 1, 2, 3 }, false)]
+    [InlineData(new[] { 1, 2, 3, 4, 5, 6, 7, 8 }, new[] { 8, 7, 6, 5, 4, 3, 2, 1 }, false)]
+    [InlineData(new[] { 1, 2, 3, 4, 5, 6, 7, 8 }, new[] { 6, 7, 8, 5, 4, 1, 2, 3 }, false)]
+    [InlineData(new[] { 1, 2, 3, 4, 5, 6, 7, 8 }, new[] { 6, 7, 8, 1, 2, 3, 4, 5 }, true)]
+    [InlineData(new[] { 6, 7, 8, 1, 2, 3, 4, 5 }, new[] { 1, 2, 3, 4, 5, 6, 7, 8 }, true)]
+    public void Given_valid_input_should_return_expected_result(int[] integerArrayOne, int[] integerArrayTwo, bool isArrayRotation)
+    {
+        var result = IsArrayRotation.IsArrayRotationSolution(integerArrayOne, integerArrayTwo);
+        Assert.Equal(isArrayRotation, result);
+    }
+}
---
+++ AlgorithmsStudy2023/IsArrayRotation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsStudy2023;
+public static class IsArrayRotation
+{
+    //BIG O - O(n) or O(2n)
+    public static bool IsArrayRotationSolution(int[] arrayA, int[] arrayB)
+    {
+        return IsRotation(arrayA, arrayB) || IsRotation(arrayB, arrayA);
+    }
+
+    private static bool IsRotation(int[] arrayA, int[] arrayB)
+    {
+        if (arrayA.Length != arrayB.Length)
+            return false;
+
+        var pointerArrayB = 0;
+
+        while (pointerArrayB < arrayB.Length)
+        {
+            var valueInA = arrayA[0];
+            var valueInB = arrayB[pointerArrayB];
+            if (valueInA != valueInB)
+            {
+                pointerArrayB++;
+                continue;
+            }
+
+            break;
+        }
+
+        //var reversedArrayB = new int[arrayB.Length];
+        var auxList = new List<int>();
+        auxList.AddRange(arrayB[pointerArrayB..]);
+        auxList.AddRange(arrayB[..pointerArrayB]);
+        var reversedArrayB = auxList.ToArray();
+
+        for (var i = 0; i < arrayA.Length; i++)
+        {
+            if (arrayA[i] != reversedArrayB[i])
+                return false;
+        }
+
+        return true;
+    }
+}
