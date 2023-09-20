605e1518d737f874aff27d0dda5a4d49a3bd1744
lucasfmarconi <lucasfmarconi@gmail.com>
2023-09-20 15:48:17 +0200

    Add nth element linked list solution

---
+++ AlgorithmsStudy2023.Tests/NthElementTests.cs
@@ -0,0 +1,26 @@
+﻿namespace AlgorithmsStudy2023.Tests;
+public class NthElementTests
+{
+    [Theory]
+    [InlineData(new[] { 7, 6, 5, 4, 3, 2, 1 }, 3, 3)]
+    [InlineData(new[] { 7, 6, 5, 4, 3, 2, 1 }, 1, 1)]
+    [InlineData(new[] { 7, 6, 5, 4, 3, 2, 1 }, 5, 5)]
+    [InlineData(new[] { 7, 6, 5, 4, 3, 2, 1 }, 7, 7)]
+    [InlineData(new[] { 1, 2, 3, 4 }, 2, 3)]
+    [InlineData(new[] { 1, 2, 3, 4 }, 4, 1)]
+    [InlineData(new[] { 1, 2, 3, 4 }, 5, 0)]
+    public void Given_valid_input_should_return_expected_result(int[] headIntegerArray, int value, int expectedReturn)
+    {
+        var countFromEnd = headIntegerArray.Length - 1;
+        var initialValue = headIntegerArray[countFromEnd..][0];
+        var current = new Node(initialValue, null);
+        for (var i = headIntegerArray.Length - 2; i >= 0; i--)
+        {
+            var newNodeValue = headIntegerArray[i];
+            current = new Node(newNodeValue, current);
+        }
+        //This logic above is to convert any array theory inline data to a Linked List
+        var result = NthElement.ReturnNthValueFromEnd(current, value);
+        Assert.Equal(expectedReturn, result);
+    }
+}
---
+++ AlgorithmsStudy2023/NthElement.cs
@@ -0,0 +1,52 @@
+﻿namespace AlgorithmsStudy2023;
+
+//Linked List
+public static class NthElement
+{
+    public static int ReturnNthValueFromEnd(Node headNode, int n)
+    {
+        var auxiliarList = new List<int>();
+
+        while (headNode.Child != null)
+        {
+            var childValue = GetNodeValueWithChild(headNode);
+            if (childValue == null)
+                return 0;
+
+            auxiliarList.Add(childValue.Value);
+            headNode = headNode.Child;
+        }
+        
+        auxiliarList.Add(headNode.Value);
+        var nodesValueArray = auxiliarList.ToArray<int>();
+        var desiredPosition = nodesValueArray.Length - n;
+
+        if (desiredPosition < 0) 
+            return 0;
+        
+        var nodeValue = nodesValueArray[desiredPosition..][0];
+        return nodeValue;
+    }
+
+    private static int? GetNodeValueWithChild(Node? node)
+    {
+        if (node?.Child != null)
+            GetNodeValueWithChild(node.Child);
+
+        var nodeValue = node?.Value;
+        return nodeValue;
+    }
+}
+
+//Node def
+public class Node
+{
+    public Node(int value, Node? child)
+    {
+        Value = value;
+        Child = child;
+    }
+
+    public int Value { get; }
+    public Node? Child { get; }
+}
