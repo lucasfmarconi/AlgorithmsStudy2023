879ce210dd0636173c586e50265b7696fc8ef37b
lucasfmarconi <lucasfmarconi@gmail.com>
2023-09-20 09:45:47 +0200

    Improve rotation solution using mod

--- AlgorithmsStudy2023/IsArrayRotation.cs
+++ AlgorithmsStudy2023/IsArrayRotation.cs
@@ -1,47 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AlgorithmsStudy2023;
+﻿namespace AlgorithmsStudy2023;
 public static class IsArrayRotation
 {
     //BIG O - O(n) or O(2n)
     public static bool IsArrayRotationSolution(int[] arrayA, int[] arrayB)
     {
-        return IsRotation(arrayA, arrayB) || IsRotation(arrayB, arrayA);
-    }
-
-    private static bool IsRotation(int[] arrayA, int[] arrayB)
-    {
         if (arrayA.Length != arrayB.Length)
             return false;
 
-        var pointerArrayB = 0;
+        var lockPointer = -1;
+        var valueInA = arrayA[0];
 
-        while (pointerArrayB < arrayB.Length)
+        for (var i = 0; i < arrayB.Length; i++)
         {
-            var valueInA = arrayA[0];
-            var valueInB = arrayB[pointerArrayB];
-            if (valueInA != valueInB)
-            {
-                pointerArrayB++;
-                continue;
-            }
+            var valueInB = arrayB[i];
+            if (valueInA != valueInB) continue;
 
+            lockPointer = i;
             break;
         }
 
-        //var reversedArrayB = new int[arrayB.Length];
-        var auxList = new List<int>();
-        auxList.AddRange(arrayB[pointerArrayB..]);
-        auxList.AddRange(arrayB[..pointerArrayB]);
-        var reversedArrayB = auxList.ToArray();
+        if (lockPointer == -1)
+            return false;
 
         for (var i = 0; i < arrayA.Length; i++)
         {
-            if (arrayA[i] != reversedArrayB[i])
+            var j = (lockPointer + i) % arrayA.Length;
+            //e.g. 3 % 8 = 3 (remainder of a division) Isn't it hacky? :)
+            
+            if (arrayA[i] != arrayB[j])
                 return false;
         }
 
