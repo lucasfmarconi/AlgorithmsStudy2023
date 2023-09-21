6ac38c077636c4a8b8308c51e5a681ab0caa00e1
lucasfmarconi <lucasfmarconi@gmail.com>
2023-09-21 13:49:23 +0200

    Simplify solution without moving pointers on original array

--- AlgorithmsStudy2023/BaseBallGame.cs
+++ AlgorithmsStudy2023/BaseBallGame.cs
@@ -5,14 +5,11 @@ public static class BaseBallGame
 {
     public static int CalPoints(string[] ops)
     {
-        int previousScoreIndex = 0, lastScoreIndex = 0;
         var scoreRecords = new List<int>();
         for (var i = 0; i < ops.Length; i++)
         {
             if (int.TryParse(ops[i], out var opScore))
             {
-                previousScoreIndex = lastScoreIndex;
-                lastScoreIndex = i;
                 scoreRecords.Add(opScore);
                 continue;
             }
@@ -24,11 +21,10 @@ public static class BaseBallGame
                     scoreRecords.Add(previousScoresSum);
                     break;
                 case "D":
-                    var doubleScore = 2 * int.Parse(ops[lastScoreIndex]);
+                    var doubleScore = 2 * scoreRecords[^1];
                     scoreRecords.Add(doubleScore);
                     break;
                 case "C":
-                    lastScoreIndex = previousScoreIndex;
                     var maxIndex = scoreRecords.Count - 1;
                     scoreRecords.RemoveRange(maxIndex, 1);
                     break;
