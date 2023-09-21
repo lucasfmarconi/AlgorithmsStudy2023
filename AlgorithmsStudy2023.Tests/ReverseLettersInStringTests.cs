c9464a1318ee83a0adc0058127930a01786741ff
lucasfmarconi <lucasfmarconi@gmail.com>
2023-09-21 14:53:12 +0200

    Add reverse letters on string solution and tests

---
+++ AlgorithmsStudy2023.Tests/ReverseLettersInStringTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsStudy2023.Tests;
+public class ReverseLettersInStringTests
+{
+    [Theory]
+    [InlineData("ab-cd", "dc-ba")]
+    [InlineData("a-bC-dEf=ghIj!!", "j-Ih-gfE=dCba!!")]
+    public void Given_valid_input_should_return_expected_result(string original, string expectedResult)
+    {
+        var result = ReverseLettersInString.ReverseLettersInStringSolution(original);
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ AlgorithmsStudy2023/ReverseLettersInString.cs
@@ -0,0 +1,35 @@
+﻿namespace AlgorithmsStudy2023;
+public static class ReverseLettersInString
+{
+    public static string ReverseLettersInStringSolution(string originalString)
+    {
+        var originalStringArray = originalString.ToCharArray();
+        var pointerOnTheLeft = 0;
+        var pointerOnTheRight = originalStringArray.Length - 1;
+        var reversedStringArray = new char[originalStringArray.Length];
+        while (pointerOnTheLeft < originalStringArray.Length)
+        {
+            var currentCharValueOnTheLeft = originalStringArray[pointerOnTheLeft];
+            if (char.IsLetter(currentCharValueOnTheLeft))
+            {
+                var currentCharValueOnTheRight = originalStringArray[pointerOnTheRight];
+                if (char.IsLetter(currentCharValueOnTheRight))
+                {
+                    reversedStringArray[pointerOnTheLeft] = currentCharValueOnTheRight;
+                    reversedStringArray[pointerOnTheRight] = currentCharValueOnTheLeft;
+                    pointerOnTheLeft++;
+                    pointerOnTheRight--;
+                    continue;
+                }
+                pointerOnTheRight--;
+                reversedStringArray[pointerOnTheRight] = currentCharValueOnTheRight;
+                continue;
+            }
+
+            reversedStringArray[pointerOnTheLeft] = currentCharValueOnTheLeft;
+            pointerOnTheLeft++;
+        }
+
+        return string.Concat(reversedStringArray);
+    }
+}
