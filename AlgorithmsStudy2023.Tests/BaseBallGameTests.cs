5f7605ddc6f2ba5cd0a561598a6fdc9ec8d3425f
lucasfmarconi <lucasfmarconi@gmail.com>
2023-09-21 13:48:10 +0200

    Add the "weird" base ball cal game solution and tests

---
+++ AlgorithmsStudy2023.Tests/BaseBallGameTests.cs
@@ -0,0 +1,13 @@
+﻿namespace AlgorithmsStudy2023.Tests;
+
+public class BaseBallGameTests
+{
+    [Theory]
+    [InlineData(new[] { "5", "2", "C", "D", "+" }, 30)]
+    [InlineData(new[] { "5", "-2", "4", "C", "D", "9", "+", "+" }, 27)]
+    public void Given_valid_input_should_return_expected_result(string[] operationsArray, int expectedResult)
+    {
+        var result = BaseBallGame.CalPoints(operationsArray);
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ AlgorithmsStudy2023/BaseBallGame.cs
@@ -0,0 +1,39 @@
+﻿namespace AlgorithmsStudy2023;
+
+//A past Turing.com problem
+public static class BaseBallGame
+{
+    public static int CalPoints(string[] ops)
+    {
+        int previousScoreIndex = 0, lastScoreIndex = 0;
+        var scoreRecords = new List<int>();
+        for (var i = 0; i < ops.Length; i++)
+        {
+            if (int.TryParse(ops[i], out var opScore))
+            {
+                previousScoreIndex = lastScoreIndex;
+                lastScoreIndex = i;
+                scoreRecords.Add(opScore);
+                continue;
+            }
+
+            switch (ops[i])
+            {
+                case "+":
+                    var previousScoresSum = scoreRecords[^1] + scoreRecords[^2];
+                    scoreRecords.Add(previousScoresSum);
+                    break;
+                case "D":
+                    var doubleScore = 2 * int.Parse(ops[lastScoreIndex]);
+                    scoreRecords.Add(doubleScore);
+                    break;
+                case "C":
+                    lastScoreIndex = previousScoreIndex;
+                    var maxIndex = scoreRecords.Count - 1;
+                    scoreRecords.RemoveRange(maxIndex, 1);
+                    break;
+            }
+        }
+        return scoreRecords.Sum();
+    }
+}
