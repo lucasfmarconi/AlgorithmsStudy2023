3e7bd4a071e484020d1c8e613e0c1ba06941b60b
lucasfmarconi <lucasfmarconi@gmail.com>
2023-09-27 15:34:42 +0200

    Add initial implementation tests for IsBinarySearchTree

---
+++ AlgorithmsStudy2023.Tests/BinarySearchTreeTests.cs
@@ -0,0 +1,59 @@
+﻿namespace AlgorithmsStudy2023.Tests;
+public class BinarySearchTreeTests
+{
+    [Fact]
+    public void Given_the_valid_binary_search_tree_method_should_return_true()
+    {
+        var givenTreeDictionary = new Dictionary<int, int[]>
+        {
+            {0, new []{1,2}},
+            {1, new []{3,4}},
+            {2, new []{5,6}}
+        };
+
+        var tree = CreateTree(givenTreeDictionary, 0);
+
+        //This logic above is to convert any array theory inline data to a Linked List / tree
+        var result = BinarySearchTree.IsBinarySearchTree(tree);
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Given_the_not_valid_binary_search_tree_method_should_return_false()
+    {
+        var givenTreeDictionary = new Dictionary<int, int[]>
+        {
+            {3, new []{1,4}},
+            {1, new []{0,2}},
+            {4, new []{5,6}}
+        };
+
+        var tree = CreateTree(givenTreeDictionary, 3);
+
+        //This logic above is to convert any array theory inline data to a Linked List / tree
+        var result = BinarySearchTree.IsBinarySearchTree(tree);
+        Assert.False(result);
+    }
+
+    private static TreeNode CreateTree(Dictionary<int, int[]> mapping, int headValue)
+    {
+        var head = new TreeNode(headValue, null, null);
+        var nodes = new Dictionary<int, TreeNode>();
+        nodes[headValue] = head;
+        foreach (var map in mapping)
+        {
+            var value = map.Value;
+            var leftChild = new TreeNode(value[0], null, null);
+            var rightChild = new TreeNode(value[1], null, null);
+            nodes[value[0]] = leftChild;
+            nodes[value[1]] = rightChild;
+        }
+
+        foreach (var map in mapping)
+        {
+            nodes[map.Key].Left = nodes[map.Value[0]];
+            nodes[map.Key].Right = nodes[map.Value[1]];
+        }
+        return head;
+    }
+}
---
+++ AlgorithmsStudy2023/BinarySearchTree.cs
@@ -0,0 +1,37 @@
+﻿namespace AlgorithmsStudy2023;
+public class BinarySearchTree
+{
+    public static bool IsBinarySearchTree(TreeNode headNodeTree)
+    {
+        if (headNodeTree.Left is null && headNodeTree.Right is null)
+            return false;
+
+        return CheckIfIsBinarySearch(headNodeTree.Left, headNodeTree.Value);
+    }
+
+    private static bool CheckIfIsBinarySearch(TreeNode headNodeTree, int headNodeValue)
+    {
+        if (headNodeTree.Left is null && headNodeTree.Right is null)
+            return true;
+
+        if (headNodeTree.Left?.Value < headNodeValue || headNodeTree.Right?.Value < headNodeValue)
+            return false;
+
+        return CheckIfIsBinarySearch(headNodeTree.Left, headNodeTree.Left.Value) && CheckIfIsBinarySearch(headNodeTree.Right, headNodeTree.Right.Value);
+    }
+}
+
+public class TreeNode
+{
+    public int Value { get; }
+    public TreeNode? Left { get; set; }
+    public TreeNode? Right { get; set; }
+
+    public TreeNode(int value, TreeNode left, TreeNode right)
+    {
+        Value = value;
+        Left = left;
+        Right = right;
+    }
+}
+
